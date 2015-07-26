e07f98b809cf374fbff6cbcac928a10f0cc089a1
Caroline Paulic <carolinepaulic@eworldes.com>
2015-07-25 18:12:34 -1000

    Added link to view resume to footer and home page

--- src/app/app.js
+++ src/app/app.js
@@ -3,4 +3,4 @@ angular.module('caroline-website',
         'ui.router',
         'caroline-website.HomeModule',
         'caroline-website.FooterModule'
-    ]); /* Module names go in array as strings */
+    ]);
