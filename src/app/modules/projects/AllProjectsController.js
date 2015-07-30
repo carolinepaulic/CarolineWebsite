b9236e08e79be5b4209cf175bee70fb360db29cf
Caroline Paulic <carolinepaulic@eworldes.com>
2015-07-29 19:13:19 -1000

    Tried to implement filter on projects page, not working so far

--- src/app/modules/projects/AllProjectsController.js
+++ src/app/modules/projects/AllProjectsController.js
@@ -1,19 +1,43 @@
 (function() {
     function AllProjectsController($scope, ProjectService, NavigationService) {
-        $scope.filters = ProjectService.getProjectFilters();
-        $scope.allProjects = ProjectService.getAllProjects();
+        $scope.filterBy = function() {
+
+        };
 
         $scope.filterBy = function(filterBy) {
-            angular.forEach($scope.filters, function(filter) {
+            console.info("here");
+/*            angular.forEach($scope.filters, function(filter) {
                 filter.iSSelected = false;
             });
 
             filterBy.isSelected = true;
+            $scope.filterBy = filteryBy.name;
+            console.info($scope.filterBy);
+
+            angular.forEach($scope.allProjects, function(project) {
+                angular.forEach(project.tags, function(tag) {
+                    if (tag == $scope.filterBy) {
+                        project.isShowing = true;
+                    }
+                });
+            });*/
         };
 
         $scope.goToProjectPage = function(project) {
             NavigationService.goToSingleProjectPage(project.id);
         };
+
+        function load() {
+            $scope.filters = ProjectService.getProjectFilters();
+            $scope.allProjects = ProjectService.getAllProjects();
+            $scope.filterBy = "All";
+
+            angular.forEach($scope.allProjects, function(project) {
+                project.isShowing = true;
+            })
+        }
+
+        load();
     }
 
     angular
