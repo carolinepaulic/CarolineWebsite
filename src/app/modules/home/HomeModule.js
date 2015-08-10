0a4af6af8cb5c39417737fe9f0919a97991e9526
Caroline Paulic <carolinepaulic@eworldes.com>
2015-08-09 19:54:24 -1000

    Made index.html default to home page

--- src/app/modules/home/HomeModule.js
+++ src/app/modules/home/HomeModule.js
@@ -1,11 +1,12 @@
 angular.module('caroline-website.HomeModule', [])
     .config(function($stateProvider, $urlRouterProvider) {
-    $stateProvider
-        .state('home', {
-            url: '/home',
-            templateUrl: 'modules/home/HomeView.html',
-            controller: 'HomeController'
-        });
+        $stateProvider
+            .state('home', {
+                url: '/home',
+                templateUrl: 'modules/home/HomeView.html',
+                controller: 'HomeController'
+            });
 
-    $urlRouterProvider.when('/', '/home');
-});
+        $urlRouterProvider.when('', '/home');
+        $urlRouterProvider.when('/', '/home');
+    });
