e24ab2cdb032e92af9e597a5e205d4e00a2a00b2
Caroline Paulic <carolinepaulic@eworldes.com>
2015-08-29 12:52:29 -1000

    Express can run app now

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/src/app')); //James added /src/app to fix this.
+app.get('*', function(req, res){
+    res.sendFile(__dirname + '/dist/src/app/index.html');
+});
 app.listen(process.env.PORT || 3000);
+console.log("App listening.");
+
+//Use angular html 5 mode
+
+/* Route url fragment #
+
+ */
